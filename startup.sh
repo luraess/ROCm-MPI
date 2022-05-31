0a64a3641d492ab50079215b0f52788ab57f5977
Ludovic Raess <ludovic.rass@gmail.com>
2022-05-31 22:45:48 +0200

    Add Pkg

--- startup.sh
+++ startup.sh
@@ -6,10 +6,12 @@ touch Project.toml
 
 julia --project -e 'using Pkg; pkg"add https://github.com/luraess/MPI.jl#lr/rocmaware-dev";'
 
+julia --project -e 'using Pkg; Pkg"add MPIPreferences";'
+
 julia --project -e 'using MPIPreferences; MPIPreferences.use_system_binary()'
 
 julia --project -e 'using Pkg; pkg"add https://github.com/luraess/ImplicitGlobalGrid.jl#mpi-dev"; pkg"add AMDGPU";'
 
-julia --project -e 'using Pkg; pkg"add Plots"'
+julia --project -e 'using Pkg; pkg"add Plots";'
 
 julia --project -e 'using Pkg; Pkg.build()'
