430b34150cdb2bd42b338bd0d4f7c1a1d8633ffd
Ludovic Räss <61313342+luraess@users.noreply.github.com>
2022-07-28 10:41:19 +0200

    Keep AP example only

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -4,6 +4,6 @@ source ./setenv.sh
 
 julia --project -O3 --check-bounds=no diffusion_2D_ap.jl
 
-julia --project -O3 --check-bounds=no diffusion_2D_kp.jl
+# julia --project -O3 --check-bounds=no diffusion_2D_kp.jl
 
-julia --project -O3 --check-bounds=no diffusion_2D_perf.jl
+# julia --project -O3 --check-bounds=no diffusion_2D_perf.jl
